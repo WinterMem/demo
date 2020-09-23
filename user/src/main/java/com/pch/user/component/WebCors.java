26adf28caafdd86d36cdd0705e215c93b116f188
WinterMem <254621340@qq.com>
2020-09-23 17:35:55 +0800

    添加日志和logback

--- user/src/main/java/com/pch/user/component/WebCors.java
+++ user/src/main/java/com/pch/user/component/WebCors.java
@@ -1,8 +1,11 @@
 package com.pch.user.component;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.pch.user.config.AsyncPorpoise;
 
 /**
  * @author: pch
@@ -18,5 +21,10 @@ public class WebCors implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
+    }
+    
+    @Bean
+    public AsyncPorpoise getAsyncPorpoise() {
+        return new AsyncPorpoise();
     }
 }
